e0bbc8f2a8ba136a3c9a3bf1ab0beb9c319448d1
Nan Du <Strideradu@users.noreply.github.com>
2018-01-21 21:07:07 -0500

    for fastq will provide estimate error rates

--- SeqLengthStat.py
+++ SeqLengthStat.py
@@ -5,10 +5,16 @@ import numpy as np
 
 def length_stat(path, type):
     length = []
+    if type == 'fastq':
+        errors = [] # expected wrong bases in each read
     with open(path) as f:
         records = SeqIO.parse(f, type)
         for record in records:
             length.append(len(record.seq))
+            if type == 'fastq':
+                q = np.array(record.letter_annotations["phred_quality"])
+                pe = 10**(-0.1 * q)
+                errors.append(length[-1] * pe)
 
     length = np.array(length)
     count = len(length)
@@ -22,6 +28,8 @@ def length_stat(path, type):
     print("The mean of reads is {}".format(mean))
     print("The median of reads is {}".format(median))
     print("longest length is {} and the shortest length is {}".format(max_len, min_len))
+    if type == 'fastq':
+        print('Estimated error rates is {}'.format(np.sum(errors)/total))
 
 if __name__ == '__main__':
 
