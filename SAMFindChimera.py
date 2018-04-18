9f4fbff41d6e997b1b57a38917063cbf45b53893
Nan Du <Strideradu@users.noreply.github.com>
2018-04-17 22:56:33 -0400

    add more stuff for process chimera read

--- SAMFindChimera.py
+++ SAMFindChimera.py
@@ -1,11 +1,17 @@
 import argparse
 import sys
 import os
+from Bio import SeqIO
 from collections import defaultdict
+import SAMparser
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="path of input file", type=str)
+    parser.add_argument("fasta", help="path of fasta file", type=str)
     parser.add_argument("output", help="path of output file", type=str)
     parser.add_argument("id_list", help="path of id list file", type=str)
 
@@ -16,21 +22,67 @@ if __name__ == '__main__':
         parser.print_help()
         sys.exit(1)
 
-    aligns = defaultdict(list)
+    aligns = defaultdict(lambda: defaultdict(list))
     with open(args.input) as f:
         for line in f:
             if line[0] != "@":
-                line = line.rstrip()
-                sp = line.split('\t')
-                if sp[2]!='*':
-                    aligns[sp[0]].append(line)
+                record = SAMparser.text_to_sam(line)
+
+                id = record.rname
+                tlen = record.tlen
+
+                if id!='*':
+                    aligns[id][tlen].append(record)
+
+    records = SeqIO.parse(args.fasta, 'fasta')
+    for record in records:
+        if record.id == '277694':
+            table_size = len(record.seq)//500 + 1
+
+    count = np.zeros((table_size, table_size))
+    sin_count = np.zeros((table_size, table_size))
+
     with open(args.output,'w') as f1:
         with open(args.id_list, 'w') as f2:
-            for key, value in aligns.items():
-                if len(value) > 2:
-                    for line in value:
-                        print(line, file=f1)
+            for key, tlen_dict in aligns.items():
+                pos = []
+                texts = []
+                is_chimera = False
+                if len(tlen_dict) > 2:
+                    is_chimera = True
 
+                for tlen, records in tlen_dict.items():
+                    if tlen > 0:
+                        if -tlen not in tlen_dict:
+                            is_chimera = True
+
+                    for record in records:
+                        pos.append(record.pos)
+                        texts.append(record.text)
+                        if record.sa:
+                            pos.append(record.sa_pos)
+                            min_pos = min(record.pos, record.sa_pos)
+                            max_pos = max(record.pos, record.sa_pos)
+
+                            if max_pos - min_pos >= 1000:
+                                sin_count[min_pos][max_pos] += 1
+
+                min_pos = np.min(pos)
+                max_pos = np.max(pos)
+
+                if max_pos - min_pos >= 1000:
+                    count[min_pos][max_pos] += 1
+
+                if is_chimera:
                     print(key, file=f2)
+                    for text in texts:
+                        print(text, file=f1)
+
+    imgplot = plt.imshow(count[:, :, 0])
+    plt.savefig("matrix_count.png")
+
+    plt.figure()
+    imgplot = plt.imshow(sin_count[:, :, 0])
+    plt.savefig("sin_matrix_count.png")
 
 
---
+++ SAMparser.py
@@ -0,0 +1,56 @@
+class sam_align(object):
+    def __init__(self):
+        self.qname = None
+        self.flag = None
+        self.rname = None
+        self.pos = None
+        self.mapq = None
+        self.cigar = None
+        self.rnext = None
+        self.pnext = None
+        self.tlen = None
+        self.seq = None
+        self.qual = None
+        self.nm = None
+
+        # secondary alignment position
+        self.sa = False
+        self.sa_rname = None
+        self.sa_pos = None
+        self.sa_strand = None
+        self.sa_cigar = None
+        self.sa_mapq = None
+        self.sa_nm = None
+
+    def parse_line(self, text):
+        self.text = text.strip()
+        sp = self.text.split("\t")
+
+        self.qname = sp[0]
+        self.flag = str(bin(int(sp[1])))
+        self.rname = sp[2]
+        self.pos = int(sp[3])
+        self.mapq = int(sp[4])
+        self.cigar = sp[5]
+        self.rnext = sp[6]
+        self.pnext = sp[7]
+        self.tlen = int(sp[8])
+        self.seq = sp[9]
+        self.qual = sp[10]
+        self.nm = int(sp[12].split(':')[2])
+
+        if len(sp) > 16:
+            self.sa = True
+            sa_sp = sp[-1].split(':')[2].split(',')
+            self.sa_rname = sa_sp[0]
+            self.sa_pos = int(sa_sp[1])
+            self.sa_strand = sa_sp[2]
+            self.sa_cigar = sa_sp[3]
+            self.sa_mapq = sa_sp[4]
+            self.sa_nm = sa_sp[5]
+
+
+def text_to_sam(text):
+    result = sam_align()
+    result.parse_line(text)
+    return result
