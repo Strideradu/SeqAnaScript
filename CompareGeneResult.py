649b716632aa0b496394eaf71045f0054b04e4b5
Nan Du <Strideradu@users.noreply.github.com>
2018-04-24 13:21:34 -0400

    add compare script

---
+++ CompareGeneResult.py
@@ -0,0 +1,34 @@
+import argparse
+import sys
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="path of input file", type=str)
+    parser.add_argument("compare", help="path of compared input file", type=str)
+
+    try:
+        args = parser.parse_args()
+
+    except:
+        parser.print_help()
+        sys.exit(1)
+
+    ref_gene_pos = {}
+    with open(args.compare) as f:
+        for line in f:
+            line = line.strip()
+            sp = line.split()
+            ref_gene_pos[sp[6]] = (int(sp[9]), int(sp[10]))
+
+
+    with open(args.input) as f:
+        for line in f:
+            line = line.strip()
+            sp = line.split()
+            gene = sp[6]
+            x = range(int(sp[9]), int(sp[10]))
+            y = range(ref_gene_pos[gene][0], ref_gene_pos[gene][1])
+            ovelap_len = len(set(x) & set(y))
+            if ovelap_len == 0:
+                print(line)
